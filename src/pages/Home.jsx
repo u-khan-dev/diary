0269c7a2ad59726b6f2888b02d920e103c8d71b1
Umar Khan <umar.khan.76801@gmail.com>
2023-05-23 18:42:33 -0400

    final

---
+++ src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import { useEffect } from 'react';
+import { usePostsContext } from '../hooks/usePostsContext.js';
+import { useAuthContext } from '../hooks/useAuthContext.js';
+import PostHead from '../components/PostHead';
+import PostForm from '../components/PostForm';
+import styles from '../styles/styles.module.scss';
+
+const Home = () => {
+    const { posts, dispatch } = usePostsContext();
+    const { user } = useAuthContext();
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch('http://localhost:4000/api/posts', {
+                headers: {
+                    'Authorization': `Bearer ${user.token}`
+                }
+            });
+            const json = await response.json();
+
+            if (response.ok) dispatch({ type: 'SET_POSTS', payload: json });
+        }
+
+        if (user) fetchPosts();
+    }, [user, dispatch]);
+
+    return (
+        <>
+            <div>
+                <h1> Posts </h1>
+
+                <ul className={styles.postList}>
+                    {posts && posts.map(post => (
+                        <PostHead key={post._id} post={post} />
+                    ))}
+                </ul>
+            </div>
+
+            <div>
+                <PostForm />
+            </div>
+        </>
+    );
+};
+
+export default Home;
