5b105f44f2b3679447996b53c3249ca2f6a09c06
Umar Khan <umar.khan.76801@gmail.com>
2023-05-23 20:52:34 -0400

    change backend for render

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -16,7 +16,7 @@ const PostForm = () => {
         };
 
         try {
-            const response = await fetch('http://localhost:4000/api/posts', {
+            const response = await fetch('https://diary-api-nbqk.onrender.com/api/posts', {
                 method: 'POST',
                 body: JSON.stringify(post),
                 headers: {
